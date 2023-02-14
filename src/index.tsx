908aa2443b8702fe040644244d144ed487e8b676
Eloi Torrents <eloitor@disroot.org>
2023-02-14 11:00:38 +0100

    Remove Intro

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,12 @@ import {Player} from "liqvid";
 import controls from "@env/controls";
 import {script} from "./markers";
 
-import Intro from "./Intro";
 import Padics from "./Padics";
 
 function Lesson() {
   /* react allows to split components  like this */
   return (
-    <Player controls={controls} script={script}>
-      <Intro data-during="intro/"/>
       <Padics data-during="padics/"/>
-    </Player>
   );
 
 }
